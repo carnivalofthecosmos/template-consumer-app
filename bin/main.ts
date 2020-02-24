709586e7c02df97245b86e95034860f843a7bfff
Timothy Purchas <timothy.purchas@iag.com.au>
2020-02-24 12:29:51 +1100

    update deps and clean up

--- bin/main.ts
+++ bin/main.ts
@@ -1,29 +1,41 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import { App } from "@aws-cdk/core";
+import { Repository as EcrRepository } from "@aws-cdk/aws-ecr";
+import { Repository as CodeRepository } from "@aws-cdk/aws-codecommit";
 import {
   ConsumerProjectStack,
   ConsumerAccountStack
 } from "@carnivalofthecosmos/core";
-import { Repository } from "@aws-cdk/aws-ecr";
-import { EnvStack } from "./env";
+import { AppNodePipeline } from "@carnivalofthecosmos/pipeline";
+import { EnvStack } from "../lib/env";
 
 const app = new App();
 
 const project = new ConsumerProjectStack(app, "TemplateApp");
 
-const ecrRepo = new Repository(project, "EcrRepo", {
+const ecrRepo = new EcrRepository(project, "EcrRepo", {
   repositoryName: `app-${project.Project}/demo`.toLowerCase()
 });
-// const codeRepo = new CodeRepo(project, "CodeRepo");
-// const codePipeline = new NodePipeline(project, "CodePipeline");
+
+const codeRepo = new CodeRepository(project, "CodeRepo", {
+  repositoryName: `app-${project.Project}-main-repo`
+});
+
+const codePipeline = new AppNodePipeline(project, "CodePipeline", {
+  name: `App-${project.Project}-Main-Pipeline`,
+  codeRepo,
+  buildCommands: ["npm run build"]
+});
 
 const account = new ConsumerAccountStack(project, "Mgt");
 
 const dev = new EnvStack(account, "Dev", {
   ecrRepo
 });
+codePipeline.addDeployEnvStage(dev, false);
 
-// const tst = new EnvStack(account, "Tst", {
-//   ecrRepo
-// });
+const tst = new EnvStack(account, "Tst", {
+  ecrRepo
+});
+codePipeline.addDeployEnvStage(tst);
