92243c8378bf8e7df4d8a434183151a8616e6745
Timothy Purchas <timothy.purchas@iag.com.au>
2020-02-25 09:46:17 +1100

    clean up

--- bin/main.ts
+++ bin/main.ts
@@ -5,16 +5,18 @@ import { AppProjectStack, AppAccountStack, AppEnvStack } from "../lib";
 
 const app = new App();
 
+// Project Level Infra
 const project = new AppProjectStack(app, "TemplateApp");
 
+// Account Level Infra
 const account = new AppAccountStack(project, "Mgt");
 
+// Dev Env Infra
 const dev = new AppEnvStack(account, "Dev");
-
-const tst = new AppEnvStack(account, "Tst");
-
-// Add  Deploy staged to code pipeline for each env
 project.CodePipeline.addDeployEnvStage(dev, {
   isManualApprovalRequired: false
 });
+
+// Tst Env Infra
+const tst = new AppEnvStack(account, "Tst");
 project.CodePipeline.addDeployEnvStage(tst);
